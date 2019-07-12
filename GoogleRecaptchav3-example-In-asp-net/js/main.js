b50c1b57e15e51a8166d5319f58f4203b1e978fc
NIHAR-SARKAR <rana.sarkar61@gmail.com>
2019-07-13 01:57:26 +0530

    Adding Private site key in we.config. changes in button click call.

--- GoogleRecaptchav3-example-In-asp-net/js/main.js
+++ GoogleRecaptchav3-example-In-asp-net/js/main.js
@@ -1,3 +1,25 @@
 (function () {
-
+    $('#btnLogin').click(function () {
+        $.ajax({
+            type: "POST",
+            url: "Default.aspx/CaptchaVerify",
+            data: {},
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (response) {
+                var _response = JSON.parse(response.d);
+                if (_response.score < 0.5) {
+                    $('#status').text("User is not human being !");
+                    $('#status').removeClass('text-info').addClass('text-danger');    
+                }
+                else {
+                    $('#status').text("User is human being .");
+                    $('#status').removeClass('text-error').addClass('text-success');
+                }
+            },
+            failure: function (response) {
+                $('#status').text("Error on Server!");
+            }
+        });
+    });
 })();
